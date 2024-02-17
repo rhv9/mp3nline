d034a502d3f313b42bb40a4c7a491d65784bafb1
rhv9 <rahulhasmukv@gmail.com>
2024-02-17 21:52:22 +0000

    Able to download locally from api.

---
+++ Backend/youtube-dl-api/YoutubeManager.cs
@@ -0,0 +1,37 @@
+﻿namespace youtube_dl_api.youtubemanager
+{
+    public class YoutubeManager
+    {
+
+        public static void GetSong(string url)
+        {
+            Console.WriteLine(url);
+
+            System.Diagnostics.Process process = new System.Diagnostics.Process();
+            System.Diagnostics.ProcessStartInfo startInfo = new System.Diagnostics.ProcessStartInfo(); 
+            startInfo.WindowStyle = System.Diagnostics.ProcessWindowStyle.Hidden;   
+            startInfo.FileName = "cmd.exe";
+            startInfo.Arguments = "/C yt-dlp.exe -x --audio-format mp3 " + url;
+            startInfo.RedirectStandardOutput = true;
+            startInfo.RedirectStandardError = true;
+            process.StartInfo = startInfo;
+            process.Start();
+
+            process.WaitForExit();
+
+            if (process.ExitCode != 0)
+            {
+                // Failed to get song do something
+
+            }
+            string output = process.StandardOutput.ReadToEnd();
+            Console.WriteLine("yt-dlp process exit code: " + process.ExitCode);
+
+            int i = 0;
+            foreach (var item in output.Split())
+            {
+                Console.WriteLine("Output message " + ++i + ": " + item);
+            }
+        }
+    }
+}
